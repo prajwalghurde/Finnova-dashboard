6d35cccb82037a37b09c43a1fd7696c82d548237
Pace__prajwal <131018809+prajwalghurde@users.noreply.github.com>
2025-04-14 10:30:24 +0530

    Update Positions.js

--- src/components/Positions.js
+++ src/components/Positions.js
@@ -9,7 +9,7 @@ const Positions = () => {
     const [allPositions,setAllPositions]=useState([]);
   
     useEffect(()=>{
-      axios.get("http://localhost:3002/allPositions").then((res)=>{
+      axios.get("hhttps://finnova-backend.onrender.com/allPositions").then((res)=>{
         console.log(res.data);
         setAllPositions(res.data);
       })
